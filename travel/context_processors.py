5f16291e102de09e2577aaffd7ecbcd50e082d7f
Hanccn <oflednumber2@gmail.com>
2026-07-04 01:41:42 +0800

    GitHub Pages deploy

--- travel/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-def amap_key(request):
-    return {
-        "AMAP_JS_KEY": getattr(settings, "AMAP_JS_KEY", ""),
-        "AMAP_JS_SECRET": getattr(settings, "AMAP_JS_SECRET", ""),
-    }
